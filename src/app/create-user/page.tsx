61667d2d9e986a6668ffadb4e96d43bdd3534244
Agustin Galante <agustin.galante.19@outlook.es>
2024-01-19 21:52:54 -0300

    handler to create user button

--- src/app/create-user/page.tsx
+++ src/app/create-user/page.tsx
@@ -1,7 +1,27 @@
 'use client'
+import { useState } from 'react'
 import Button from '../components/ui/button'
+import useFakeSession from '../store/fakeSession'
 
 function CreateUser() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { changeTag, tag } = useFakeSession()
+
+  const handleUserCreation = () => {
+    setIsLoading(true)
+    fetch('/api/create-user', {
+      method: 'POST',
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        localStorage.setItem('vctag', response.data)
+        changeTag(response.data)
+      })
+      .catch((err) => console.log('error on user creation', err))
+      .finally(() => setIsLoading(false))
+  }
+
   return (
     <div className="mt-32">
       <div className="flex flex-col items-center">
@@ -9,9 +29,16 @@ function CreateUser() {
         <p className="dark:text-gray-300 text-gray-700">
           To start an conversation you have to create an random id
         </p>
-        <Button size="normal" className="mt-2" disabled variant="secondary">
+        <Button
+          size="normal"
+          className="mt-2"
+          variant="secondary"
+          isLoading={isLoading}
+          onClick={handleUserCreation}
+        >
           Create User
         </Button>
+        <span className="text-xl mt-4 p-2 bg-gray-100/10 rounded-md"># {tag}</span>
       </div>
     </div>
   )
